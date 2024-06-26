3184862313444b009aa8a1d34f23d643704ee25f
左龙 <2458404536@qq.com>
2024-06-26 10:18:24 +0800

    0.0

--- sendpy_03.py
+++ sendpy_03.py
@@ -2,7 +2,9 @@ from web3 import Web3
 
 # 自定义配置
 #rpc_url = "http://127.0.0.1:26657"  # 自定义的 RPC URL
-rpc_url ="https://airchains-rpc.kubenode.xyz/"
+#rpc_url ="https://airchains-rpc.kubenode.xyz/"
+
+rpc_url = "http://127.0.0.1:8545"
 chain_id = 1234  # 自定义的链 ID
 
 # 钱包地址和私钥
